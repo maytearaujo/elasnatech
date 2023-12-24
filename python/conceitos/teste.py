826f2e89837310d3373af14535dc751a4d75ba7f
Maytê <maytearaujo@yahoo.com.br>
2023-12-24 08:17:13 -0300

    Video del

--- python/conceitos/teste.py
+++ python/conceitos/teste.py
@@ -1,3 +1,3 @@
-frase = 'Concluir os cursos de C#, realizar projetos relevantes, subir os projetos para o GitHub e realizar postagem no linkedin a cada conquista para permitir que recrutadores visualizem o progresso do meu aprendizado e aumentem as minhas chances de contratação.'
+frase = 'Concluir os cursos de Python, realizar projetos relevantes, subir os projetos para o GitHub e realizar postagem no linkedin a cada conquista para permitir que recrutadores visualizem o progresso do meu aprendizado e aumentem as minhas chances de contratação.'
 
 print(len(frase))
